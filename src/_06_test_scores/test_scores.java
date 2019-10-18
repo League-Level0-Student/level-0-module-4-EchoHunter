bf19222acab10d4bb7a143d6fdfe4f1dd9700da2
League Student <league@iMac-11.attlocal.net>
2019-10-17 18:29:58 -0700

    :man_with_gua_pi_mao:

--- src/_06_test_scores/test_scores.java
+++ src/_06_test_scores/test_scores.java
@@ -1,7 +1,28 @@
 package _06_test_scores;
 
+import javax.swing.JOptionPane;
+
 public class test_scores {
 	public static void main(String[] args) {
+	String score =JOptionPane.showInputDialog("what did you get on your most recent test?");
+Double Scoredec =	Double.parseDouble(score);
+	if((Scoredec > (80.00))&&(Scoredec <(90.00))) {
+		JOptionPane.showMessageDialog(null, "Congrats, you did mediocre!");
+	}
+	else	if(Scoredec > (90.01)) {
+		JOptionPane.showMessageDialog(null, "Congrats, You did MAGNIFICEDMT:JKLFLAKSJDIUGH:OWML:FHP:AJWD:LMC:HEF:OAJLd");
+	}
+	else if((Scoredec > (69.01))&&(Scoredec < (79.99))) {
+		JOptionPane.showMessageDialog(null, "you did meh. :( ");
 		
+	}
+	else if(Scoredec .equals(69.00)) {
+		JOptionPane.showMessageDialog(null, "nice ");
+	}
+	else if (Scoredec < (68.99)) {
+		JOptionPane.showMessageDialog(null, "You did terribly, redo kindergarten now!");
+		
+	}
+	
 	}
 }
--- src/_07_sleepy_head/SleepyHead.java
+++ src/_07_sleepy_head/SleepyHead.java
@@ -1,30 +1,47 @@
 package _07_sleepy_head;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
+
 //    Level 0
 
 import javax.swing.JOptionPane;
 
-
 public class SleepyHead {
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        
-        boolean isWeekday, isVacation;
-        
-        /* You MUST use the above boolean variables in your code */
-        
-        /*
-         * Ask the user for these values using a confirm dialog like the one below
-         * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
-         */
-    
+		boolean isWeekday, isVacation;
 
+		/* You MUST use the above boolean variables in your code */
 
-        /*
-         * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
-         * print “get up lazybones!�? If it is a weekday, and we are on vacation,
-         * print “sleep in�?.
-         */
-    }
+		/*
+		 * Ask the user for these values using a confirm dialog like the one below
+		 * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head",
+		 * JOptionPane.YES_NO_OPTION);
+		 */
+		int i = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+		
+		int i1 = JOptionPane.showConfirmDialog(null, "Is it a vacation.?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+		isWeekday = i==0;
+		isVacation = i1==0;
+
+		
+		if (isVacation&&isWeekday) {
+			JOptionPane.showMessageDialog(null, "SLEEP IN LAZYBONES");
+
+		}
+		else if(!isVacation&&isWeekday) {
+			JOptionPane.showMessageDialog(null, "get up, LAZYBONES");
+		}
+		else if (isVacation&&!isWeekday) {
+			JOptionPane.showMessageDialog(null, "SLEEP IN LAZYBONES");
+		}
+		else if(!isVacation&&!isWeekday) {
+			JOptionPane.showMessageDialog(null, "SLEEP IN LAZYBONES");
+		}
+		/*
+		 * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday, print
+		 * “get up lazybones!�? If it is a weekday, and we are on vacation, print “sleep
+		 * in�?.
+		 */
+	}
 }
