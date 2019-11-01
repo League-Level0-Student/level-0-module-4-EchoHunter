ffae3240f56f5e20a10c494f626508df1605468d
League Student <league@iMac-12.attlocal.net>
2019-10-31 18:29:11 -0700

    :b:
    pi aloid, sting methos hapy peet

--- src/_10_happy_pet/HappyPet.java
+++ src/_10_happy_pet/HappyPet.java
@@ -5,17 +5,17 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+String pet = JOptionPane.showInputDialog("What pet do you want, a rock, a dog, a platypus, or , Barack Obama");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			int task = JOptionPane.showOptionDialog(null, "What do you want to do with your pet?", "Pet time", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "feed", "water", "walk","pet" ,"let it die" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
 
@@ -27,4 +27,30 @@ public class HappyPet {
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+	
+void HappinessNoise(){
+happinessLevel++;
+JOptionPane.showMessageDialog(null, "your pet makes a happy noise");
+JOptionPane.showMessageDialog(null, "your happiness level increased to "+ happinessLevel);
+}
+void MUNCH() {
+	happinessLevel++;
+	JOptionPane.showMessageDialog(null, "your pet commits MUNCH");
+	JOptionPane.showMessageDialog(null, "your happiness level increased to "+ happinessLevel);
+}
+void EXERCISE() {
+	happinessLevel++;
+	JOptionPane.showMessageDialog(null, "your pet commits MUNCH");
+	JOptionPane.showMessageDialog(null, "your happiness level increased to "+ happinessLevel);
+	
+	
+}
+
+
+
+
+
+
+
+}
 }
